e810f79437f125bbf994fdfc5272e5543314bff2
Ziqin Wang <wangzq2017@mail.sustech.edu.cn>
2019-12-01 20:02:44 +0800

    Improve testing

--- test/diff_test.py
+++ test/diff_test.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
+import logging
 import os
 import subprocess
+import sys
 
-for filename in os.listdir():
-    if filename.endswith('.ac'):
-        prefix = filename[:-3]
-        assert subprocess.run(['../bin/splc', prefix + '.spl']).returncode in (0, 4)
-        subprocess.run(['diff', filename, prefix + '.out'], check=True)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
+    
+    passed = True
+    for filename in os.listdir():
+        if filename.endswith('.ac'):
+            prefix = filename[:-3]
+            logging.info(f'testing {prefix}')
+            try:
+                splc_return_code = subprocess.run(['../bin/splc', prefix + '.spl']).returncode
+                assert splc_return_code in (0, 4)
+                subprocess.run(['diff', filename, prefix + '.out'], check=True)
+            except AssertionError:
+                logging.error(f'  failed: unexpected exit status (code={splc_return_code})')
+                passed = False
+            except subprocess.CalledProcessError:
+                logging.error(f'  failed: wrong output')
+                passed = False
+    if not passed:
+        sys.exit('tests failed')
